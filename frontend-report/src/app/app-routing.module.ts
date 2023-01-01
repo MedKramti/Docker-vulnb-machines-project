ed97a1b5c1c5fb2fb5ae5081e1b4ed2cc6839edd
Kramti <mohamedkramti99@gmail.com>
2023-01-01 19:40:44 +0100

    Frontend - Created setup page

--- frontend-report/src/app/app-routing.module.ts
+++ frontend-report/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SetupComponent } from './setup/setup.component';
 
-const routes: Routes = [{ path: 'bruteforce' }];
+const routes: Routes = [{ path: 'setup', component: SetupComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
